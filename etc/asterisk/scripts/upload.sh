8116faa08782076ba348e82dbab295f3712523b2
Rich Jones <rich@anomos.info>
2011-07-03 18:13:07 -0400

    upload from bash

--- etc/asterisk/scripts/upload.sh
+++ etc/asterisk/scripts/upload.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 # You must have vorbis-tools installed for this to work.
-filename="{$1}.wav"
-oggenc $filename
-filename="{$1}.ogg"
+oggenc ${1}.wav
+filename=$1.ogg
 number=$2
 areacode=${number:0:3}
-fileparam="rec_file=@${filename}"
+fileparam="rec_file=@$filename"
 nameparam="name=VoiceDropboxSubmission:${areacode}"
 publicparam="public_description=${areacode}"
 privateparam="private_description=NONE"
 locparam="location=${areacode}"
 
 `curl -F ${fileparam} -F ${publicparam} -F ${nameparam} -F ${privateparam} -F ${locparam} http://openwatch.net/uploadnocaptcha/`
+
